bd433de0d8ed0f4d6e289cb95e6868fa8c70b174
gediont <teshomegedion@gmail.com>
2020-01-01 09:38:49 +0300

    minor fix, entry and interface remains

--- src/Admin.java
+++ src/Admin.java
@@ -6,10 +6,10 @@ public class Admin extends Employee {
     private String password;
 
     // default constructor
-       public Admin(String firstName, String middleName, String lastName, String sex, String nationality, String city, String subcity, String woreda, String place_of_birth, Date date_of_birth, Date start_date, String maritalStatus, String educationLevel, Double salary, String phone, String email, String username, String password) {
+       public Admin(String firstName, String middleName, String lastName, String sex, String nationality, String city, String subcity, String woreda, String place_of_birth, Date date_of_birth, Date start_date, String maritalStatus, String educationLevel, Double salary, String phone, String email, String usrname, String pswd) {
         super(firstName, middleName, lastName, sex, nationality, city, subcity, woreda, place_of_birth, date_of_birth, start_date, maritalStatus, educationLevel, salary, phone, email);
-        this.username = username;
-        this.password = password;
+        this.username = usrname;
+        this.password = pswd;
     }
 
     // setter methods
@@ -26,17 +26,15 @@ public class Admin extends Employee {
         return username;
     }
 
+    public String getPassword() {
+           return password;
+    }
+
     // authenticate an admin
     public boolean authenticate(String username, String Password) {
          boolean flag = false;
             // search from file and authenticate using flag
         return flag;
-    }
-
-    // to create a new Admin account
-    public static void main(String[] args) {
-
-
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,118 @@
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
+
+
 public class Main {
-    public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//        ArrayList<Employee> TestOne = new ArrayList<Employee>();
-//        System.out.print("How many users do you want to register now?");
-//        int n = input.nextInt();
+     static Scanner input = new Scanner(System.in);
 
-        menu();
+     // Main Method to perform all operations
+    public static void main(String[] args) throws ParseException {
+        boolean entry = auth();
 
+        if(entry) {
+            menu();
 
+        }
+        else{
+            System.out.println("Authorization failed, good-bye");
+        }
+
+    }
+
+    // method to send authorization signal
+    static boolean auth() throws ParseException {
+        System.out.println("\n\n Authorization");
+        System.out.println(" ==================== ");
+        System.out.println("1. Already Have an Admin Account");
+        System.out.println("2. Create a new Admin Account");
+
+        int choice =input.nextInt();
+
+        switch (choice) {
+            case 1:
+                    System.out.print("Enter username: ");
+                    String username = input.next();
+                    System.out.print("Enter password: ");
+                    String password = input.next();
+
+                    //check if it's in the file of admin list
+
+                    // return true;
+            case 2:
+                System.out.println("Enter information");
+                System.out.print("First name: ");
+                String firstName = input.next();
+//                validateString(firstName);
+                System.out.print("Middle name: ");
+                String middleName = input.next();
+//                validateString(middleName);
+                System.out.print("Last name: ");
+                String lastName = input.next();
+//                validateString(lastName);
+                System.out.print("Sex: ");
+                String sex = input.next();
+//                validateString(sex);
+                System.out.print("Nationality: ");
+                String nationality = input.next();
+//                validateString(nationality);
+                System.out.print("City: ");
+                String city = input.next();
+//                validateString(city);
+                System.out.print("Sub-city: ");
+                String subcity = input.next();
+//                validateString(subcity);
+                System.out.print("Woreda: ");
+                String woreda = input.next();
+//                validateString(woreda);
+                System.out.print("Place of Birth: ");
+                String place_of_birth = input.next();
+//                validateString(place_of_birth);
+                System.out.print("\nDate of Birth (DD/MM/YYYY): ");
+                String DoB = input.next();
+                Date date_of_birth = null;
+                try {
+                    date_of_birth = new SimpleDateFormat("dd/MM/yyyy").parse(DoB);
+                } catch (ParseException e) {  System.out.print("Message: " + e); }
+
+                System.out.print("\nStarted Working on (DD/MM/YYYY: ");
+                String stDate = input.next();
+                Date start_date = new SimpleDateFormat("dd/MM/yyyy").parse(stDate);
+                System.out.print("\nMarital Status (Married, Single, Engaged): ");
+                String maritalStatus = input.next();
+//                validateString(maritalStatus);
+                System.out.print("\nEducation Level: (BSC/Msc/PhD): ");
+                String educationLevel = input.next();
+//                validateString(educationLevel);
+                System.out.print("\nPhone :");
+                String phone = input.next();
+//                validatePhone(phone);
+                System.out.print("\nE-mail: ");
+                String email = input.next();
+//                validateEmail(email);
+                System.out.print("\nSalary: ");
+                Double salary = input.nextDouble();
+                System.out.print("\nEnter new Username: ");
+                String usrname = input.next();
+                System.out.print("\nEnter new Password: ");
+                String pswd = input.next();
+                System.out.println("\n\n===========================\n");
+
+                Admin newEntrant = new Admin(firstName, middleName, lastName, sex, nationality, city, subcity, woreda, place_of_birth, date_of_birth, start_date, maritalStatus, educationLevel, salary
+                , phone, email, usrname, pswd);
+
+                System.out.println("+++++++++++++++++++++++++++++");
+                System.out.println("Account Created Successfully");
+                auth(); break;
+            default:
+                System.out.print("Wrong Selection, try agian");
+                auth(); break;
+            }
+
+       return false;
     }
 
     static void menu() {
@@ -25,7 +126,6 @@ public class Main {
         System.out.println("\t 3. Search Instructor by Name");
         System.out.println("\t 4. Search Instructor by Id");
         System.out.println("\t 5. Sort Instructors by Name");
-        System.out.println("\t 6. List All Instructors");
-
+        System.out.println("\t 6. List All Instructors \n\n");
     }
 }
